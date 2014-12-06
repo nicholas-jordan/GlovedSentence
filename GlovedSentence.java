72c3c9845eeaec9033778747aec56240f23404d1
Nicholas Jordan <nicholas-jordan@users.noreply.github.com>
2014-12-06 17:01:04 -0600

    Update GlovedSentence.java

--- GlovedSentence.java
+++ GlovedSentence.java
@@ -1,9 +1,25 @@
 package vectorized_text;
 //
-class GlovedSentence
+import com.sun.corba.se.impl.io;
+//
+// My idea being to do some study of word and sentence lengths 
+// in both cryllic / american-english with possible side-attributes 
+// of a readily available study of Unihan which is a built-in for Java Character 
+// this well-accomplished study can be utilized within the context of what is called CORBA
+// 
+// Note that is not Cobra.
+//
+// Which are docile compared to our enemy.
+//
+// Which fires at 77 Kilometers elevation 
+//
+// Heat Tiles blocking due to Ionization Levels near some temperatures of the Solar Exosphere.
+// 
+//
+class GlovedSentence extends ObjectStreamField
 {
-    GlovedSentence()
+    GlovedSentence(final java.lang.String string)
     {
-        ;
+        super(new String("GlovedSentence"),java.lang.Class.forName("GlovedSentence"));
     }
 }
